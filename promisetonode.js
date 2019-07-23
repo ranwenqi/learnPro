a5ee3abe4e5a8bde4be1eb59188b2d5ce40675b1
ranwenqi <337061694@qq.com>
2019-07-23 14:12:23 +0800

    in conpany commit

---
+++ promisetonode.js
@@ -0,0 +1,36 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (request, response) {
+
+    // 发送 HTTP 头部
+    // HTTP 状态值: 200 : OK
+    // 内容类型: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // 发送响应数据 "Hello World"
+    response.end('Hello World\n');
+}).listen(8888);
+
+// fs.readFile('get与post的区别.txt','utf-8', function(err, data) {
+//     if (err) {
+//         console.log(err);
+//     }else {
+//         console.log(data.toString())};
+// })
+
+const promisefy = function(file) {
+    return new Promise((resolve,reject)=>{
+        fs.readFile(file, 'utf-8', function(err, data) {
+            if (err) return reject(err);
+            return resolve(data);
+        });
+    })
+}
+promisefy('get与post的区别.txt').then(data => {
+    console.log(data);
+});
+
+
+// 终端打印如下信息
+console.log('Server running at http://127.0.0.1:8888/');
